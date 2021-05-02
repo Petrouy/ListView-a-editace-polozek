6eec76a27dd206daaa793f9f5baa6b06afc1419d
Petrouy <petros3014@gmail.com>
2021-05-02 22:46:27 +0200

    Přidejte soubory projektu.

---
+++ ListView a editace položek/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListView_a_editace_položek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            University.InitUniversities();
+            University u = new University();
+            DataContext = u;
+        }
+
+
+        private void Universities_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            University ui = (University)((sender as ListView).SelectedItem);
+            DataContext = ui;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Universities.SelectedIndex = 0;
+        }
+
+        private void Universities_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            University ui = (University)((sender as ListView).SelectedItem);
+            UniversityEdit ue = new UniversityEdit(ui);
+            ue.ShowDialog();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            UniversityEdit ue = new UniversityEdit();
+            ue.ShowDialog();
+        }
+    }
+
+}
---
+++ ListView a editace položek/University.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListView_a_editace_položek
+{
+    public class University : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set { name = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Name")); }
+        }
+
+        private string state;
+        public string State
+        {
+            get => state;
+            set { state = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("State")); }
+        }
+
+        private int established;
+        public int Established
+        {
+            get => established;
+            set { established = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Established")); }
+        }
+
+        private string address;
+        public string Address
+        {
+            get => address;
+            set { address = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Address")); }
+        }
+
+        private double successRate;
+        public double SuccessRate
+        {
+            get => successRate;
+            set { successRate = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SuccessRate")); }
+        }
+
+        private decimal tuition;
+        public decimal Tuition
+        {
+            get => tuition;
+            set { tuition = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Tuition")); }
+        }
+
+        public static ObservableCollection<University> AllUniversities { get; set; } = new ObservableCollection<University>();
+
+        public static void InitUniversities()
+        {
+            AllUniversities.Add(new University
+            {
+                Name = "JeVidětŽe",
+                State = "JsemNestíhal",
+                Established = 22091991
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "Viktor",
+                State = "Skála",
+                Established = 02042001
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "David",
+                State = "Obruč",
+                Established = 12121987
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "Viktorie",
+                State = "Navrátilová",
+                Established = 07082000
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "Vojta",
+                State = "Kuděk",
+                Established = 09121999
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "Martin",
+                State = "Bezruč",
+                Established = 05081989
+            });
+
+            AllUniversities.Add(new University
+            {
+                Name = "Tereza",
+                State = "Jurášková",
+                Established = 20042003
+            });
+        }
+
+    }
+}
